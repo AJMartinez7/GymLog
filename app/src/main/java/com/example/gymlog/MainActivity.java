6021e6970b6d0a7ad9a0394ebeb52eb23d5e0730
AJMartinez7 <ajmartinez88@yahoo.com>
2025-11-29 02:46:03 -0800

    End of video 6:

--- app/src/main/java/com/example/gymlog/MainActivity.java
+++ app/src/main/java/com/example/gymlog/MainActivity.java
@@ -1,20 +1,24 @@
 package com.example.gymlog;
 
 import android.os.Bundle;
+import android.provider.CallLog;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.gymlog.database.GymLogRepository;
+import com.example.gymlog.database.entities.GymLog;
 import com.example.gymlog.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    ActivityMainBinding binding;
-
+    private ActivityMainBinding binding;
+    private GymLogRepository repository;
     public static final String TAG = "DAC_GYMLOG";
     String exercise = "";
     double weight= 0.0;
@@ -26,22 +30,40 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        repository = GymLogRepository.getRepository(getApplication());
+
         binding.logDisplayTextView.setMovementMethod(new ScrollingMovementMethod());
+
+        updateDisplay();
 
         binding.logButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getInformationFromDisplay();
+                insertGymLogRecord();
                 updateDisplay();
             }
         });
     }
 
+    private void insertGymLogRecord() {
+        if (exercise.isEmpty()) {
+            return;
+        }
+        GymLog log = new GymLog(exercise, weight, reps);
+        repository.insertGymLog(log);
+    }
+
     private void updateDisplay() {
-        String currentInfo = binding.logDisplayTextView.getText().toString();
-        Log.d(TAG, "current info: " + currentInfo);
-        String newDisplay = String.format(Locale.US,"Exercise:%s%nWeight:%.2f%nReps:%d%n=-=-=-=%n%s", exercise, weight, reps, currentInfo);
-        binding.logDisplayTextView.setText(newDisplay);
+        ArrayList<GymLog> allLogs = repository.getAllLogs();
+        if (allLogs.isEmpty()) {
+            binding.logDisplayTextView.setText(R.string.nothing_to_show_time_to_hit_the_gym);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (GymLog log : allLogs) {
+            sb.append(log);
+        }
+        binding.logDisplayTextView.setText(sb.toString());
     }
 
     private void getInformationFromDisplay() {
@@ -58,6 +80,5 @@ public class MainActivity extends AppCompatActivity {
         } catch (NumberFormatException e) {
             Log.d(TAG, "Error reading value from reps edit text.");
         }
-
     }
 }
